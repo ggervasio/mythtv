fbb277461da9d5540be71f1cc8cc03b4bcb1b8fb
Gregorio Gervasio, Jr <ggervasio@yahoo.com>
2010-12-02 19:43:15 -0800

    Added ability to scale CC608 display.

--- mythtv/programs/mythfrontend/globalsettings.cpp
+++ mythtv/programs/mythfrontend/globalsettings.cpp
@@ -1896,6 +1896,16 @@ static HostComboBox __attribute__ ((unused)) *DecodeVBIFormat()
     return gc;
 }
 
+static HostSpinBox *OSDCC608TextZoomPercentage(void)
+{
+    HostSpinBox *gs = new HostSpinBox("OSDCC608TextZoom", 50, 200, 5);
+    gs->setLabel(QObject::tr("VBI caption text zoom percentage"));
+    gs->setValue(100);
+    gs->setHelpText(QObject::tr("Use this to enlarge or shrink captions."));
+
+    return gs;
+}
+
 static HostSpinBox *OSDCC708TextZoomPercentage(void)
 {
     HostSpinBox *gs = new HostSpinBox("OSDCC708TextZoom", 50, 200, 5);
@@ -4164,6 +4174,7 @@ OSDSettings::OSDSettings()
     osd->addChild(DefaultCCMode());
     osd->addChild(PreferCC708());
     osd->addChild(SubtitleFont());
+    osd->addChild(OSDCC608TextZoomPercentage());
     osd->addChild(OSDCC708TextZoomPercentage());
     osd->addChild(SubtitleCodec());
     //osd->addChild(UDPNotifyPort());
