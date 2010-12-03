1d25e06e5e24a667db813b1353473e7e56c4c2fe
Gregorio Gervasio, Jr <ggervasio@yahoo.com>
2010-12-02 19:25:25 -0800

    Changed composer key to writer.

--- mythtv/external/FFmpeg/libavformat/mov.c
+++ mythtv/external/FFmpeg/libavformat/mov.c
@@ -145,7 +145,7 @@ static int mov_read_udta_string(MOVContext *c, ByteIOContext *pb, MOVAtom atom)
     case MKTAG(0xa9,'n','a','m'): key = "title";     break;
     case MKTAG(0xa9,'a','u','t'):
     case MKTAG(0xa9,'A','R','T'): key = "artist";    break;
-    case MKTAG(0xa9,'w','r','t'): key = "composer";  break;
+    case MKTAG(0xa9,'w','r','t'): key = "writer";  break;
     case MKTAG( 'c','p','r','t'):
     case MKTAG(0xa9,'c','p','y'): key = "copyright"; break;
     case MKTAG(0xa9,'c','m','t'):
