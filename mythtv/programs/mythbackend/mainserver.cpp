3e93249061debd97778cb17c003f7eda6272d15f
Gregorio Gervasio, Jr <ggervasio@yahoo.com>
2010-12-02 19:19:40 -0800

    Added logging for file paths.

--- mythtv/programs/mythbackend/mainserver.cpp
+++ mythtv/programs/mythbackend/mainserver.cpp
@@ -5757,6 +5757,7 @@ void MainServer::SetExitCode(int exitCode, bool closeApplication)
 QString MainServer::LocalFilePath(const QUrl &url, const QString &wantgroup)
 {
     QString lpath = url.path();
+    VERBOSE(VB_FILE, QString("Raw local file path: %1").arg(lpath));
 
     if (lpath.section('/', -2, -2) == "channels")
     {
@@ -5783,6 +5784,7 @@ QString MainServer::LocalFilePath(const QUrl &url, const QString &wantgroup)
     else
     {
         lpath = lpath.section('/', -1);
+        VERBOSE(VB_FILE, QString("Basename file path: %1").arg(lpath));
 
         QString fpath = lpath;
         if (fpath.right(4) == ".png")
