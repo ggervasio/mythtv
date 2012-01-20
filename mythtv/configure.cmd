7efcba074f9efa5691630f43a94fc4b7b3a501ef
Gregorio Gervasio, Jr <ggervasio@yahoo.com>
2012-01-19 17:57:05 -0800

    Cleaned up.  Added VAAPI.

--- mythtv/configure.cmd
+++ mythtv/configure.cmd
@@ -1,4 +1,4 @@
-./configure --libdir-name=lib64 --enable-firewire --enable-proc-opt --with-bindings=perl --enable-opengl-video
+./configure --libdir-name=lib64 --enable-proc-opt --enable-vaapi
 
 # --extra-cflags=-g --extra-cxxflags=-g
 # --enable-liba52 --enable-liba52bin --disable-decoder=ac3
