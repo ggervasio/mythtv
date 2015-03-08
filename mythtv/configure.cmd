cf4fad55422ac5fdbcc89f51be20f0e075f22267
Gregorio Gervasio, Jr <gregorio.gervasio@gmail.com>
2015-03-08 16:27:37 -0700

    Forced cpu to core2.

--- mythtv/configure.cmd
+++ mythtv/configure.cmd
@@ -1,4 +1,4 @@
-./configure --libdir-name=lib64 --enable-proc-opt --enable-vaapi
+./configure --libdir-name=lib64 --enable-proc-opt --enable-vaapi --cpu=core2
 
 # --extra-cflags=-g --extra-cxxflags=-g
 # --enable-liba52 --enable-liba52bin --disable-decoder=ac3
